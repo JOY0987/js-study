5226870e4112b76a43534cd4ba2ac4e3adb5e946
JOY0987 <jjoyewon@gmail.com>
2023-03-16 09:17:24 +0900

    2023-03-16 다시정리

--- 1. 자바스크립트 기초/6. quiz1.js
+++ 1. 자바스크립트 기초/6. quiz1.js
@@ -1,5 +1,28 @@
-// var total = 0;
+// 4 ~ 7 까지의 누적합
 
-// while
+var total = 0;
+var n = 4;
 
-// console.log(`4 ~ 7까지의 누적합: ${total}`);
+while (n <= 7) {
+    total += n;
+    n++;
+}
+
+console.log(`4 ~ 7까지의 누적합: ${total}`);
+
+
+
+// x ~ y 까지의 누적합
+
+var x = +prompt('첫번째 숫자를 입력!');
+var y = +prompt('두번째 숫자를 입력!');
+
+var total = 0;
+var n = x;
+
+while (n <= y) {
+    total += n;
+    n++;
+}
+
+console.log(`${x} ~ ${y}까지의 누적합: ${total}`);
--- 1. 자바스크립트 기초/7. quiz2.js
+++ 1. 자바스크립트 기초/7. quiz2.js
@@ -1,2 +1,18 @@
-var a = prompt('첫번째 숫자를 입력!');
-var b = prompt('두번째 숫자를 입력!');
+// 입력받은 양의 정수만큼 + - 반복 출력
+// 2-2. 홀수번째 반복에는 +가 붙어야하고
+//      짝수번째 반복에는 -가 붙어야함
+
+var n = +prompt('양의 정수를 입력!');
+var x = 1;
+
+while (x <= n) {
+    if(x % 2 !== 0) {
+        resultMark += '+';
+    } else {
+        resultMark += '-';
+    }
+
+    x++;
+}
+
+alert(resultMark);
