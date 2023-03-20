13a300dd7c130ddfd8ee6ad553ec378f2835bcc3
JOY0987 <jjoyewon@gmail.com>
2023-03-20 10:59:12 +0900

    프로퍼티

--- 3. 객체/1. 객체 기초.js
+++ 3. 객체/1. 객체 기초.js
@@ -11,12 +11,6 @@ var dog = {
     playWithChild: function() {}
 };
 
-// if 나 for의 {}블록은 값이 아니지만, 객체 리터럴은 값이기 때문에 {} 뒤에 세미콜론을 붙여줌
-// 키에 예약어 사용 가능
-// 문자열로 처리하면 띄어쓰기도 가능
-// 위에 작성한 것처럼 모든 것이 프로퍼티가 될 수 있음
-// 순서보다(배열) 데이터를 표현하는 명시적인 이름이 필요할 때 객체 리터럴을 사용함
-
 var cat = {
     name: '콩순이',
     age: 2,
@@ -42,3 +36,119 @@ dog.favorite.slice(1).splice(1,1); // 가능, 사본을 가져와서 splice, 메
 console.log(dog);
 
 
+
+// 프로퍼티 참조 2
+console.log('===================================');
+console.log(dog.injection);
+console.log(dog['injection']);
+
+// key를 변수에 저장
+var fv = 'favorite';
+console.log(cat.fv); // undefined
+console.log(cat[fv]); // [ '낮잠자기', '방어지럽히기' ]
+
+
+
+// 프로퍼티 값 수정
+dog.age = 4;
+cat.favorite[1] = '실뭉치';
+
+dog['injection'] = false;
+
+
+
+// 프로퍼티 동적 추가
+cat.owner = '김철수'; // owner 키가 없었으니 추가가 됨
+cat.owner = '박영희'; // 수정
+console.log(cat);
+
+// 프로퍼티 삭제
+delete cat.owner;
+console.log(cat);
+
+
+// 프로퍼티 존재 유무 확인
+// 수정하기 전에 있는지 확인하고 진행해야함
+console.log('===================================');
+
+// key를 반드시 문자열로 표기해야함!!
+var ageFlag = 'age' in cat; // true or false
+console.log(ageFlag);
+
+// master라는 프로퍼티가 없으면 추가해라 (존재 유무 확인방법)
+if (!('master' in cat)) {
+    cat['master'] = '김또또';
+}
+
+
+
+// 객체를 순회하는 반복문
+console.log('===================================');
+
+// key를 반복해서 추출해줌
+for (var k in dog) {
+    // console.log(k); // undefined
+    // console.log(typeof k); // string
+    console.log(dog[k]); // 문자열 키를 참조할 때는 꼭 대괄호를 붙임
+}
+
+
+
+
+//객체의 중첩구조
+//예시 : 게시판
+
+var articles = {
+    totalCount: 25578,  //총 게시물 수
+    admin: 'abc1234', //게시판 관리자 아이디
+    page: 2558, //총 페이지 수
+    articleList: [ // 게시물 목록
+        {
+            bno: 3, //글번호
+            title: '가위바위보', //글제목
+            writer: '김짱껨뽀',  //작성자
+            content: '다덤벼 ^^', //글내용
+            viewCount: 53, //조회수
+            recomm: 10, //추천수
+            regDate: '21-12-07' //작성일자
+        }, // 0번 index 
+        {
+            bno: 2, //글번호
+            title: '노는게', //글제목
+            writer: '뽀로로',  //작성자
+            content: '제일조와~~~', //글내용
+            viewCount: 253, //조회수
+            recomm: 11, //추천수
+            regDate: '21-12-06' //작성일자
+        }, // 1번 index
+        {
+            bno: 1, //글번호
+            title: '아멘', //글제목
+            writer: '개신교신자',  //작성자
+            content: '할렐루야', //글내용
+            viewCount: 23, //조회수
+            recomm: 5, //추천수
+            regDate: '21-12-05' //작성일자
+        } // 2번 index
+    ]
+};
+
+
+console.log(articles.totalCount); 
+console.log(articles.articleList[1].writer); 
+    // articleList는 배열, articleList[1]은 객체!!
+
+// 변수에 저장해서 사용도 가능
+var bbs = articles.articleList;
+bbs[0].writer;
+
+// key의 개수만큼 반복
+for (var x in articles) {
+    // 4번 반복
+}
+for (var x in articles.articleList[0]) { 
+    // 7번 반복
+}
+for (var y of bbs) {
+    // 3번 반복
+}
